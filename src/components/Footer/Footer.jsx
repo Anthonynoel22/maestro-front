63f70fd358fa12885638ba8c0182a8516581a3c6
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-10-22 15:34:38 +0200

    footer statique + scss wip

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="#">Nous contacter</a></li>
+                    <li><a href="#">Politique de confidentialité</a></li>
+                    <li><a href="#">Informations légales</a></li>
+                    <li><a href="#">CGU</a></li>
+                    <li><a href="#">Accessibilité</a></li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
