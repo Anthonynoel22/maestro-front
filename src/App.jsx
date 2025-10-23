0a4f20806f52f0d7faa6ba2d27d77c95e7470b79
CynthiaManier <cynthia.manier.pro@gmail.com>
2025-10-23 16:12:33 +0200

    ClientList component with bootstrap - in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 /* import Header from './components/Header/Header.jsx' */
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx';
-import Description from './components/Description/Description'
-import PreviewList from './components/PreviewList/PreviewList.jsx'
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Description from "./components/Description/Description";
+import PreviewList from "./components/PreviewList/PreviewList.jsx";
+import ClientList from "./components/ClientList/ClientList.jsx";
 
-import './index.css';
+import "./index.css";
 
 function App() {
-
-  return (
-    <div className='App'>
-        {/* <Header /> */}
-        <Header />
-        <main>
-          <Description />
-          <PreviewList />
-        </main>
-        <Footer />
-          
-          
-    </div>
-  );
-
+    return (
+        <div className="App">
+            {/* <Header /> */}
+            <Header />
+            <main>
+                {/* <Description /> */}
+                {/* <PreviewList /> */}
+                <ClientList />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
-export default App 
+export default App;
