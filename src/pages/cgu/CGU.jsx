63b861da822d445853746795bcfc85d1ca9a0e8f
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-11-20 13:46:37 +0100

    pages cgu et accessibiliy in progress

--- src/pages/cgu/CGU.jsx
+++ src/pages/cgu/CGU.jsx
@@ -1,48 +1,50 @@
-import "./CGU.scss"
+import "./CGU.scss";
 
 function CGU() {
 
 return (
-    <>
-            <div className='container-header'>
+    <div>
+    <div className="cgu-container-header">
+            
                 <h2>CGU</h2>
                     <p>En vigueur au 00/00/00</p>
                         <p>Bienvenue sur notre site Maestro. Veuillez lire attentivement nos conditions générales d’utilisation avant d’utiliser notre site.</p>
             </div>
 
-                
-                    <h3>1.Édition du site</h3>
+                <div className="cgu-container-main">
+                    <h3>1.Édition du site :</h3>
                     <p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>
 
 
-                    <h3>2.Hébergement</h3>
+                    <h3>2.Hébergement :</h3>
                     <p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>
 
 
-                    <h3>3.Accès Au Site</h3>
+                    <h3>3.Accès Au Site :</h3>
                     <p>Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si, exempli</p>
 
 
-                    <h3>4.Propriété Intellectuelle</h3>
+                    <h3>4.Propriété Intellectuelle :</h3>
                     <p>Quod Enchiridion Epictetus stoici scripsit. Rodrigo Abela et Technologiae apud Massachusetts instituta Opera collectio.</p>
 
 
-                    <h3>5.Responsabilité</h3>
+                    <h3>5.Responsabilité :</h3>
                     <p>Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam</p>
 
 
-                    <h3>6.Droit Application et Juridiction</h3>
+                    <h3>6.Droit Application et Juridiction :</h3>
                     <p>Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam</p>
 
 
-                    <h3>7.Droit Application et Juridiction Compétente</h3>
+                    <h3>7.Droit Application et Juridiction Compétente :</h3>
                     <p>Si ergo illa tantum fastidium compesce contra naturalem usum habili, quem habetis vestra potestate, non aliud quam</p>
 
 
-                    <h3>Contact</h3>
-                    <p>Pour toute question relative aux présentes CGU, l’utilisateur peut nous contacter via la page Contact</p>
+                    <h3>Contact :</h3>
+                    <p>Pour toute question relative aux présentes CGU, l’utilisateur peut nous contacter via la page Nous Contact</p>
+                </div>
                 
-                </>
+                </div>
     );
 }
 
