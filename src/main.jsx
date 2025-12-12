a220cfae373c0c63732685a7a9258212ebcdb951
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-12-12 13:11:36 +0100

    reset css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+/* import "bootstrap/dist/css/bootstrap.min.css";
+import "normalize.css"; */
+import "./index.scss";
 import ToastProvider from "./components/Toast/Toast.jsx";
 
 createRoot(document.getElementById("root")).render(
