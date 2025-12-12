9e0a5efa6368d7f1f50e03a8133bc9cbaef76855
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-12-12 13:16:28 +0100

    some test

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-/* import "bootstrap/dist/css/bootstrap.min.css";
-import "normalize.css"; */
-import "./index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ToastProvider from "./components/Toast/Toast.jsx";
 
 createRoot(document.getElementById("root")).render(
